abcca104f1265cb7c6a724c1999721eb387f17ad
Puria Nafisi Azizi <puria@dyne.org>
2019-07-12 11:54:16 +0200

    :loud_sound: Result as a msg playload

--- zencode-node.js
+++ zencode-node.js
@@ -1,11 +1,16 @@
 module.exports = function(RED) {
     function ZencodeNode(config) {
-		const zenroom = require('zenroom')
+        const zenroom = require('zenroom')
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-			zenroom.default.script(msg.payload).zenroom_exec()
+            const script = msg.payload
+            let result = ''
+            zenroom.default.script(script).print((o) => { result = o }).zenroom_exec()
+            msg = {payload: result}
             node.send(msg);
+            node.log("Zenroom node executed")
+            node.status({fill:"green",shape:"dot",text:"executed"});
         });
     }
     RED.nodes.registerType("zencode", ZencodeNode);
