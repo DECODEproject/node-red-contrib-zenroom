aa0642830cea0bd4625d305e9a80d3b1d0e31580
Puria Nafisi Azizi <puria@dyne.org>
2019-07-16 14:12:51 +0200

    :construction: WIP nodes for working with zenroom nodes

---
+++ data-node.js
@@ -0,0 +1,16 @@
+module.exports = function(RED) {
+  function ZencodeDataNode(config) {
+    this.source = config.source;
+    this.name = config.name;
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    node.on("input", function(msg) {
+      const script = node.source;
+      msg.payload = msg.payload || script;
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("DATA", ZencodeDataNode);
+};
--- zencode-node.js
+++ zencode-node.js
@@ -1,27 +1,46 @@
 module.exports = function(RED) {
-    function ZencodeNode(config) {
-        const zenroom = require('zenroom')
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            const script = msg.payload
-            let result = ''
-            zenroom.default.script(script)
-                           .print(o => { result = o })
-                           .success(() => {
-                                msg = {payload: result}
-                                node.send(msg);
-                                node.log("Zenroom node executed")
-                                node.status({fill:"green",shape:"dot",text:"executed"});
-                           })
-                           .error(() => {
-                                node.log("Zenroom node executed with error")
-                                node.status({fill:"red",shape:"ring",text:"errors on execution"});
-                                node.error(result, msg)
-                           })
-                           .zenroom_exec()
+  function ZencodeNode(config) {
+    this.source = config.source;
+    this.name = config.name;
+    const zenroom = require("zenroom").default;
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-        });
-    }
-    RED.nodes.registerType("zencode", ZencodeNode);
-}
+    node.on("input", function(msg) {
+      const script = node.source;
+      let result = "";
+      let error = "";
+
+      zenroom
+        .script(script)
+        .print(o => { result = o; })
+        .success(() => {
+          node.send({ payload: result });
+          setSuccess(node);
+        })
+        .error(() => {
+          node.error(error, msg);
+          setFailure(node);
+        })
+        .zenroom_exec();
+    });
+  }
+
+  function setSuccess(node) {
+    node.status({
+      fill: "green",
+      shape: "dot",
+      text: "executed"
+    });
+  }
+
+  function setFailure(node) {
+    node.status({
+      fill: "red",
+      shape: "ring",
+      text: "errors on execution"
+    });
+  }
+
+  RED.nodes.registerType("zencode", ZencodeNode);
+};
